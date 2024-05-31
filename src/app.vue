b34b89cd0d8a1d2674ae6bc51ef629dcd711e266
Tom Siouan <tomsiouan@icloud.com>
2024-05-31 14:40:13 +0200

    chore: add a color theme switcher

--- src/app.vue
+++ src/app.vue
@@ -3,11 +3,9 @@
 </script>
 
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -57,3 +57,7 @@ export default {
   },
   plugins: [],
 }
+
+module.exports = {
+  darkMode: 'class',
+}
