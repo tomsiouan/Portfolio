4de6019ca9bf48fe0fa3b3028b1c24555bb672e2
Tom Siouan <tomsiouan@icloud.com>
2024-05-31 14:11:43 +0200

    chore: add a language switcher

---
+++ src/components/global/langSwitcher.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const {locales, locale, setLocale} = useI18n();
+
+const language = computed({
+  get: () => locale.value,
+  set: (value) => setLocale(value),
+});
+</script>
+
+<template>
+  <select v-model="language">
+    <option
+      v-for="item in locales"
+      :key="typeof item === 'object' ? item.code : item"
+      :value="item.code"
+      >{{ item.name }}
+    </option>/
+  </select>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/portfolio/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {NuxtLink} from "#components";
+
+definePageMeta({
+  layout: 'portfolio'
+});
+
+const localPath = useLocalePath();
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">
+    {{ $t("portfolio-home-title") }}
+  </h1>
+
+  <h3 class="">
+    {{ $t("portfolio-home-subtitle") }}
+  </h3>
+  <NuxtLink :to="localPath('/')" class="color-blue">Retourner à l'accueil</NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
