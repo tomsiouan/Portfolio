274633cac4c628c9fd4471b4e955d105b9993346
Tom Siouan <tomsiouan@icloud.com>
2024-06-01 11:58:19 +0200

    chore(style): langSwitcher.vue

--- src/components/global/langSwitcher.vue
+++ src/components/global/langSwitcher.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
-const {locales, locale, setLocale} = useI18n();
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { locales, locale, setLocale } = useI18n();
 
 const language = computed({
   get: () => locale.value,
   set: (value) => setLocale(value),
 });
+
+const hoveredLocale = ref(null);
+
+const handleMouseEnter = (code) => {
+  hoveredLocale.value = code;
+};
+
+const handleMouseLeave = () => {
+  hoveredLocale.value = null;
+};
 </script>
 
 <template>
-  <select v-model="language">
-    <option
-      v-for="item in locales"
-      :key="typeof item === 'object' ? item.code : item"
-      :value="item.code"
-      >{{ item.name }}
-    </option>/
-  </select>
+  <div>
+    <button
+        v-for="(item, index) in locales"
+        :key="typeof item === 'object' ? item.code : item"
+        :class="[
+        'px-2 py-1 transition duration-200',
+        language === item.code ? (hoveredLocale !== null && hoveredLocale !== item.code ? 'bg-white text-gray-800' : 'bg-gray-800 text-white') : 'bg-white text-gray-800',
+        language !== item.code && 'hover:bg-gray-800 hover:text-white',
+        index === 0 ? 'rounded-l-lg' : '',
+        index === locales.length - 1 ? 'rounded-r-lg' : ''
+      ]"
+        @click="language = item.code"
+        @mouseenter="handleMouseEnter(item.code)"
+        @mouseleave="handleMouseLeave"
+    >
+      {{ item.name }}
+    </button>
+  </div>
 </template>
 
 <style scoped>
-
+/* Ajout de styles personnalisés si nécessaire */
 </style>
