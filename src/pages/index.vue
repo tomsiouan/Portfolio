b7d256c08eb9b69c9cf0e09d4d71a12e07248832
Tom Siouan <tomsiouan@icloud.com>
2024-06-01 13:12:33 +0200

    chore: instant redirect on portfolio.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { NuxtLink } from '#components'
 
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+
 const localPath = useLocalePath();
 
+const router = useRouter();
+
+onMounted(() => {
+  router.push('/portfolio');
+});
 </script>
 
 <template>
   <div>
     <h1>{{ $t("home-title") }}</h1>
-    <h3>{{$t("home-subtitle")}}</h3>
+    <h3>{{$t("home-subtitle")}}</h3>s
     <NuxtLink :to="localPath('/portfolio')" class="color-blue">Home</NuxtLink>
   </div>
 </template>
