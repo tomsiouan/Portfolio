8c55f22e8a548e75c51439559f5499cf0c8c288b
Tom Siouan <tomsiouan@icloud.com>
2024-06-01 21:13:38 +0200

    chore(portfolio footer): set footer.

--- src/components/portfolio/portfolio.footer.vue
+++ src/components/portfolio/portfolio.footer.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
-  <h1>Je suis le footer du portfolio</h1>
+  <footer class="bg-zinc-900 text-white text-sm leading-relaxed py-10 mt-16">
+    <div class="container mx-auto max-w-screen-xl">
+      <div class="mb-8">
+        <div>
+          <p>Computer sciences student</p>
+          <p>Based in Belfort(90), France</p>
+          <p>I am currently working on side projects in addition to those for school.</p>
+        </div>
+      </div>
+      <div class="footer-lower flex justify-between md:flex-row flex-col gap-4">
+        <small>website made using nuxt, tailwindcss</small>
+        <small>tomsiouan.fr 2024 ©</small>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
