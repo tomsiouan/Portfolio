fc84c4aaf5d857f2456943b776eb543e159b6d34
Tom Siouan <tomsiouan@icloud.com>
2024-06-01 12:18:44 +0200

    chore(layout portfolio): add dark: transition bg and color.

--- src/layouts/portfolio.vue
+++ src/layouts/portfolio.vue
@@ -3,11 +3,19 @@
 </script>
 
 <template>
-  <PortfolioNavbar />
-  <slot />
-  <PortfolioFooter />
+  <div class="dark:bg-gray-80 dark:text-tertiary theme-transition text-transition">
+    <PortfolioNavbar />
+    <slot />
+    <PortfolioFooter />
+  </div>
 </template>
 
 <style scoped>
+.theme-transition {
+  transition: background-color 0.5s, color 0.5s;
+}
 
+.text-transition {
+  transition: color 0.5s, color 0.5s;
+}
 </style>
